77206240e178927f6d0cde7ba3bf41c0bd833f77
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-04 17:28:20 +0500

    get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
@@ -12,10 +13,5 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post topilmadi.")
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html',{'post': post})
