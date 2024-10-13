e7274907c4d58a717de5158ffa5940aed331bded
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-13 11:38:46 +0500

    Creating SEO-friendly URLs for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = 'blog'
 urlpatterns = [
     path('post/', post_list, name='post_list'),
-    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,29 +12,10 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, id=pk, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'detail.html', {'post': post})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
