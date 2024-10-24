5c430de80ae3b12d38340419c61b65236f504829
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-25 01:28:41 +0500

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -50,7 +50,14 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # form for users to comment
     form = CommentForm()
-    return render(request, 'detail.html', {'post': post, 'comments': comments, 'form': form})
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request, 'detail.html', {'post': post,
+                                           'comments': comments,
+                                           'form': form,
+                                           'similar_posts':similar_posts})
 
 
 def post_share(request, post_id):
