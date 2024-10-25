bae56c91e5e91d2dd8f610c79050245204d16630
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-25 15:39:48 +0500

    postgresql

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'blog',
+'USER': 'ismoilkhan22',
+'PASSWORD': '22',
 }
+}
+
 
 
 # Password validation
