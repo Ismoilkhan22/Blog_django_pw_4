832b024ba26504e35c54218a99f282396e3457d0
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-10-25 18:57:16 +0500

    [200~

--- blog/views.py
+++ blog/views.py
@@ -107,10 +107,11 @@ def post_search(request):
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
-        search_vector = SearchVector('title', 'body')
-        search_query = SearchQuery(query)
+        search_vector = SearchVector('title', weight='A')+\
+            SearchVector('body', weight='B')
+        search_query = SearchQuery(query, config='spanish')
         results = Post.published.annotate(
-        search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+        search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
     return render(request,
     'search.html',
     {'form': form,
